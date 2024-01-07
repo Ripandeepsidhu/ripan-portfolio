5f6f83c434a1e2d7ceda88c33780f583d6be3c1a
Ripandeepsidhu <kaursidhuripandeep@gmail.com>
2024-01-07 22:24:20 +0100

    bars done

--- src/components/About.js
+++ src/components/About.js
@@ -10,20 +10,26 @@ import coding from '../images/code-6127616_1280.webp';
 const About = () => {
     return(
     
-        <div className='intro'>
-        <div>
-        <p className='title'>EDUCATION</p>
-        </div> 
+        <div className='about'>
+        <p className='title'>EDUCATION</p> 
         <div className='education-bars'>
             <div className='education-bar'>
                 <img src={graduate} alt='' className='graduate'/>
+                <div className='education-bar-text'>
+                <p className='study'>Bachelor's in Computer Applications</p>
+                <p className='edu-para'>I have completed this degree from Baba Farid College, India.</p>
+            </div>
             </div>
             <div className='education-bar'>
             <img src={coding} alt='' className='graduate'/>
+            <div className='education-bar-text'>
+                <p className='study'>IT Tech for women</p>
+                <p className='edu-para'>It's a frontend development course that i have studied from sundsgården folkhögskola, Helsingborg.</p>
+            </div>
             </div>
             
         </div>
-        <div className='divider'></div>
+        <div className='divider2'></div>
        {/* <img src={htmlpicture} alt='' className='logo'/>
         <img src={csspicture} alt='' className='logo'/>
         <img src={jspicture} alt='' className='logo'/>
